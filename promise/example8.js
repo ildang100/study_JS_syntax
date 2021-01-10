c785da6222014e6bb52f26d9b8fb835b711944a6
ildang100 <last2599@gmail.com>
2021-01-11 01:59:47 +0900

    Promise 학습

---
+++ promise/example8.js
@@ -0,0 +1,10 @@
+// p 라는 프로미스 객체가 fulfilled 되는 시점에 p.then 안에 설정한 callback 함수가 실행 됩니다.
+
+const p = new Promise((resolve, reject) => {
+    // pending
+    setTimeout(() => {
+        resolve(); // fulfilled
+    }, 1000)
+});
+
+p.then(/* callback */)
