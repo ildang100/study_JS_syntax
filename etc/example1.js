d751592cb612245b4eb6128527cd78181c9c8a01
ildang100 <last2599@gmail.com>
2021-01-14 23:01:16 +0900

    JS 에서 true false 판정

---
+++ etc/example1.js
@@ -0,0 +1,15 @@
+
+// true
+console.log(!undefined)
+console.log(!null);
+console.log(!0);
+console.log(!'')
+console.log(!NaN);
+console.log(!false);
+
+// false
+console.log(!3);
+console.log(!'hello')
+console.log(!['array']);
+console.log(![])
+console.log(!{})
