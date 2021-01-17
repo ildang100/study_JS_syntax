9619a753d1ba955bdbf8ffb2c9f54801fff315e7
ildang100 <last2599@gmail.com>
2021-01-17 22:10:43 +0900

    Promise.all 학습
    이 전의 순차적인 실행은 시간을 기다렸지만,
    
    promise.all 을 사용하면 모두다 실행하고, 배열에 순서에 맞게 값이 담겨져있어, 시간을 단축키실수있다.

---
+++ sync/example4.js
@@ -0,0 +1,29 @@
+function sleep(ms) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve()
+        },ms)
+    })
+}
+
+const getDog = async () => {
+    await sleep(1000);
+    return '멍멍이'
+}
+
+const getRabbit = async () => {
+    await sleep(500)
+    return '깡통깡통'
+}
+
+const getTurtle = async () => {
+    await sleep(3000)
+    return '거북이'
+}
+
+async function process() {
+    const results = await Promise.all([getDog(), getRabbit(), getTurtle()])
+    console.log(results)
+}
+
+process();
